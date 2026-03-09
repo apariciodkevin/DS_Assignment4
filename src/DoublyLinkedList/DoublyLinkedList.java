183f97b7dd6eb386034bf2edbe745813a7887503
KevnAp <apariciodkevin@Gmail.com>
2026-03-08 23:12:33 -0500

    added initial methods

--- src/DoublyLinkedList/DoublyLinkedList.java
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -27,23 +27,77 @@ public class DoublyLinkedList {
         }
     }
 
-    public void addFirst(){
-
-    }
-
-    public void addlast(){
+    public void addFirst(Object val){
         Node newNode = new Node(val);
 
-
+        if (size == 0){
+            head = newNode;
+            tail = newNode;
+            size++;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+            size++;
+        }
 
     }
 
-    public void add(){
+    public void addLast(Object val){
+        Node newNode = new Node(val);
 
+        if (size == 0){
+            addFirst(val);
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+            tail.next = null;
+            size++;
+        }
+    }
+
+    public String toString(){
+        if(head == null) {
+            return "";
+        }
+        Node temp = head;
+        String str = "[";
+        while (temp.next != null){
+            str += temp.data + " | ";
+            temp = temp.next;
+        }
+        str += temp.data;
+        return str + "]";
+    }
+
+    public void add(int index, Object val){
+        if (index == 0){
+            addFirst(val);
+        }
+        if (index == size){
+            addLast(val);
+        } else {
+            Node temp1 = head;
+            for (int i = 0; i < index;i++){
+                temp1 = temp1.next;
+            }
+
+            Node temp2 = temp1.next;
+
+            Node newNode = new Node(val);
+            temp1.next = newNode;
+            newNode.prev = temp1;
+            newNode.next = temp2;
+            temp2.prev = newNode;
+            size++;
+        }
     }
 
     public void deleteFirst(){
+        if (size == 0){
 
+        }
     }
 
     public void deleteLast(){
@@ -54,8 +108,12 @@ public class DoublyLinkedList {
 
     }
 
-    public int get(){
-
+    public Object get(int index){
+        Node n = head;
+        for (int i=0; i < index; i++){
+            n = n.next;
+        }
+        return n;
     }
 
     public int size(){
